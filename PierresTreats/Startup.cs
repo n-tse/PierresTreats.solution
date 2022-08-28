d7595e4f60ba2f18487a406df685a73e651242fd
Nick Tse <nicholashtse@gmail.com>
2022-08-28 00:04:36 -0700

    add all controllers

--- PierresTreats/Startup.cs
+++ PierresTreats/Startup.cs
@@ -4,10 +4,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Treats.Models;
+using PierresTreats.Models;
 using Microsoft.AspNetCore.Identity;
 
-namespace Treats
+namespace PierresTreats
 {
   public class Startup
   {
@@ -26,11 +26,11 @@ namespace Treats
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<TreatsContext>(options => options
+        .AddDbContext<PierresTreatsContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
 
       services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<TreatsContext>()
+                .AddEntityFrameworkStores<PierresTreatsContext>()
                 .AddDefaultTokenProviders();
       
       // overrides Identity's pw defaul setting
